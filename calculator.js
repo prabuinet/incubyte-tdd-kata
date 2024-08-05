c70fc672e33fe544a591b75a7632aee7d23467d0
prabu <prabuinet@gmail.com>
2024-08-05 15:52:39 +0530

    bases cases passing

--- calculator.js
+++ calculator.js
@@ -1,6 +1,13 @@
 
 function add(numlist) {
+    if(typeof numlist !== "string") {
+        return 0;
+    }
 
+    if(numlist.trim() === "")
+        return 0;
+
+    return 0;
 }
 
 module.exports = add;
--- calculator.test.js
+++ calculator.test.js
@@ -3,3 +3,10 @@ const add = require('./calculator')
 test('should return 0 for empty string', () => {
    expect(add('')).toBe(0); 
 });
+
+test('should return 0 for anything other than a string', () => {
+    expect(add(null)).toBe(0); 
+    expect(add(undefined)).toBe(0); 
+    expect(add({})).toBe(0); 
+});
+
